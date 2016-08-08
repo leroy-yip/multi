a2e8721c3435c59405596dbdc1ac7e9a25a54f47
demo2 <lyip@le.com>
2016-08-08 10:52:28 -0700

    just do it

--- multi1/src/main/java/artifactory/test/Multi1.java
+++ multi1/src/main/java/artifactory/test/Multi1.java
@@ -5,6 +5,6 @@ package artifactory.test;
  */
 public class Multi1 {
     public static void main(String[] args) {
-        System.out.println("Hello World01!");
+        System.out.println("Hello World02!");
     }
 }
